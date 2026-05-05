78cd73af5222a15ccc9d6d90ff9343e9fab40a94
Sara ꪔ̤̫‬ꪔ̤̱ꪔ̤̮ <yl6025@columbia.edu>
2026-05-05 13:55:26 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * Caches core assets for offline support and installability.
  */
 
-const CACHE_NAME = "portable-attention-box-v24";
+const CACHE_NAME = "portable-attention-box-v25";
 const CORE_ASSETS = [
   "./",
   "./index.html",
